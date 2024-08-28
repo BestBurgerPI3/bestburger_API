b44a25368e7215eb3023fef1e8ab4d3a591ba949
Andres Felipe Gamez Florez <90786575+ZazAndres@users.noreply.github.com>
2024-08-28 17:42:38 -0500

    config

---
+++ app.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import routes from './routes/routes.js';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import path from 'path';
+import { PORT, HOSTNAME } from './config.js'
+
+const app = express();
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use('/bestburger', routes);  
+
+app.use(express.static(path.resolve("public")));
+
+http.createServer(app).listen(PORT, HOSTNAME, () => {  
+    console.log(`Server running at http://${HOSTNAME}:${PORT}/`);
+});
