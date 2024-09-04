bca87c78d1d256821d34113139fb6137ce400c07
Andres Felipe Gamez Florez <90786575+ZazAndres@users.noreply.github.com>
2024-09-03 20:46:34 -0500

    feat: Arreglo Routes

--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,7 @@
 import { Router } from 'express'
 import usuarioController from '../controllers/usuarioController.js';
 import usuarioAccionesController from '../controllers/usuarioAccionesController.js'; 
+import { productController } from '../controllers/controllers.js';
 const router = Router();
 
 router.post('/REGISTER_USER', usuarioController.register);
