73e256931c4165ea0121e9539fb85dd448cfda64
Santiadi <88754625+Santiadi@users.noreply.github.com>
2024-10-16 19:51:00 -0500

    Revert "Dev"

--- app.js
+++ app.js
@@ -4,18 +4,13 @@ import http from 'http';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import path from 'path';
-import { fileURLToPath } from 'url';
 import { PORT, HOSTNAME } from './config.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 const app = express();
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
-app.use('/bestburger', routes);
-app.use("/icon", express.static(path.join(__dirname, './icons')));
-
+app.use('/bestburger', routes);  
 
 app.use(express.static(path.resolve("public")));
 
