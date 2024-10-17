a679e4ce09a19c7b844adb33427d9c613f91d5ae
Andres Felipe Gamez Florez <90786575+ZazAndres@users.noreply.github.com>
2024-10-16 22:17:58 -0500

    feat: Fix COmentario

--- controllers/usuarioAccionesController.js
+++ controllers/usuarioAccionesController.js
@@ -38,12 +38,12 @@ export default class usuarioAccionesController {
     }
     static async comentarHambuguesa(req, res) {
         try {
-            // const token = req.headers.authorization?.split(' ')[1];
-            // const decoded = jwt.verify(token, JWT_SECRET);
-            // const { Correo } = decoded;
-            // console.log('Correo del usuario: ' + Correo);
+            const token = req.headers.authorization?.split(' ')[1];
+            const decoded = jwt.verify(token, JWT_SECRET);
+            const { Correo } = decoded;
+            console.log('Correo del usuario: ' + Correo);
             
-            const { Correo, Descripcion, Calificacion, Imagen, idLugar, idHamburguesa } = await req.body;
+            const { Descripcion, Calificacion, Imagen, idLugar, idHamburguesa } = await req.body;
 
             const response = await MODEL.comentarHamburguesa( Descripcion, Calificacion, Imagen ,idLugar, idHamburguesa, Correo);
             
